5d8d6510ec5d6d712f63d89294e0ae976eeea82a
Ibokan <>
2014-04-23 14:49:52 +0800

    Initial Commit

---
+++ UI高级测试/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UI高级测试
+//
+//  Created by Ibokan on 14-4-23.
+//  Copyright (c) 2014年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
